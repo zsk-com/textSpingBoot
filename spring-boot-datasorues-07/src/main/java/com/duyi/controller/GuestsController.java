83bdef92bd09f2a7478a5b172a505a537af77617
zsk-com <1263294312@qq.com>
2020-04-21 22:29:33 +0800

    MyBatis事务和多数据源

--- spring-boot-datasorues-07/src/main/java/com/duyi/controller/GuestsController.java
+++ spring-boot-datasorues-07/src/main/java/com/duyi/controller/GuestsController.java
@@ -6,7 +6,6 @@ import com.duyi.service.GuestsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.interceptor.TransactionAspectSupport;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
